2a4d6dc7e68d8f51f57824ab4149e876ffea7e7a
Velmoren <velmoren@yahoo.com>
2024-11-17 16:54:26 +0300

    refactoring

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,46 +1,5 @@
-// Счетчик на главной странице
-import { CountUp } from './countUp';
+import {countersModule} from './modules/counters'
+import {animateModule} from './modules/animate'
 
-window.onload = function () {
-  const statsFlags = new Set();
-
-  const startCountUp = (element) => {
-    const id = element.id;
-    const endValue = parseInt(element.dataset.value, 10);
-
-    if (statsFlags.has(id)) return;
-
-    const countUp = new CountUp(id, endValue, {
-      enableScrollSpy: false,
-      duration: 4,
-    });
-
-    countUp.start();
-    statsFlags.add(id);
-  };
-
-  const observerOptions = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.5,
-  };
-
-  const observerCallback = (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        startCountUp(entry.target);
-      }
-    });
-  };
-
-  const observer = new IntersectionObserver(observerCallback, observerOptions);
-
-  document.querySelectorAll('[data-value]').forEach((element) => {
-    observer.observe(element);
-  });
-};
-
-// Задержка анимации
-
-import WOW from 'wow.js';
-new WOW().init();
+countersModule()
+animateModule()
