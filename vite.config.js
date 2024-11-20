a091fde60906a8a4b4ad08a2d237a8dc7a481268
Velmoren <velmoren@yahoo.com>
2024-11-21 00:09:49 +0300

    add partner page

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         trading: resolve(__dirname, 'trading.html'),
+        partner: resolve(__dirname, 'partner.html'),
       },
     },
   },
